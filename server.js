1324bc749c593bb789008bb9d055fe2b96ee233f
differentsyntax <nothingreallyfaraway@gmail.com>
2020-04-19 06:04:45 -0400

    serve html pages

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 const port = 3000;
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
+    response.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'));
 });
 
 app.listen(port, () => {
